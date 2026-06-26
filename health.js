e3e6cc3e5a4d070bc3666a130a4e6a5213b0d66e
chun12300 <a0919091113@gmail.com>
2026-06-26 10:25:18 +0800

    Update health.js

--- health.js
+++ health.js
@@ -1,1 +1,32 @@
+// ===================================
+// 人生RPG
+// Health Module v1.0
+// ===================================
 
+// ------------------------------
+// 返回 Dashboard
+// ------------------------------
+
+const backBtn=document.getElementById("backBtn");
+
+if(backBtn){
+
+    backBtn.onclick=function(){
+
+        location.href="dashboard.html";
+
+    };
+
+}
+
+// ------------------------------
+// 運動紀錄
+// ------------------------------
+
+const saveExercise=document.getElementById("saveExercise");
+
+if(saveExercise){
+
+    console.log("Health Module Loaded");
+
+}
